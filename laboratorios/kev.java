65ab1e0225e5d7788c81f73d9c82ef108c4f2a5a
Unknown <kevingallardo_24@hotmail.com>
2016-12-15 18:59:36 -0500

    kevprueba
    kevprrueba

---
+++ laboratorios/kev.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by kgallardo on 12/12/2016.
+ */
+
+
+
+class kev {
+       public static void main(String[ ] args) {
+                                Scanner  sc = new Scanner(System.in);
+                   String st = sc.nextLine ();
+               }
+           }
+
+
